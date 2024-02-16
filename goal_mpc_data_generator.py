e5e9563de9632a0a9f2bd187f4c4b765a2d3c09d
Jonathan Lee <93632168+MrActis@users.noreply.github.com>
2024-02-16 17:51:49 +0000

    smaller data generator

--- goal_mpc_data_generator.py
+++ goal_mpc_data_generator.py
@@ -4,37 +4,38 @@ from tqdm import tqdm
 from goal_mpc_node import solve_mpc
 
 
-## input state mesh grid parameters
-# car velocity [30]
+## input state mesh grid parameters [>1.1 billion]
+# car velocity [18]
 v_car_min = -1.0
 v_car_max =  8.0
-dv_car =     0.3
+dv_car =     0.5
 
-# goal x-position [104]
+# goal x-position [52]
 x_goal_min = -1.2
 x_goal_max =  4.0
-dx_goal =     0.05
+dx_goal =     0.1
 
-# goal y-position [80]
+# goal y-position [40]
 y_goal_min = 0.0
 y_goal_max = 4.0
-dy_goal =    0.05
+dy_goal =    0.1
 
-# goal heading [126]
+# goal heading [63]
 t_goal_min = -3.14
 t_goal_max =  3.14
-dt_goal =     0.05
+dt_goal =     0.1
 
-# goal velocity [30]
+# goal velocity [18]
 v_goal_min = -1.0
 v_goal_max =  8.0
-dv_goal =     0.3
+dv_goal =     0.5
 
 
 def main(args=None):
     n_jobs = 45  # number of jobs
 
     # generate input state mesh grid
+    print('Generating input state mesh grid')
     v_car = np.arange(v_car_min, v_car_max + dv_car, dv_car)
     x_goal = np.arange(x_goal_min, x_goal_max + dx_goal, dx_goal)
     y_goal = np.arange(y_goal_min, y_goal_max + dy_goal, dy_goal)
@@ -48,26 +49,41 @@ def main(args=None):
     y_goal = y_goal_m.flatten()
     t_goal = t_goal_m.flatten()
     v_goal = v_goal_m.flatten()
+    print('Input state mesh grid generation completed:', len(v_car))
 
     # generate MPC lookup table
-    full_table = None
-    interval = 100000000
+    # print('Importing previous checkpoint')
+    # full_table = np.load('goal_mpc_lookup_table.npz')['table']
+    # print('Checkpoint import completed')
+    # print(len(v_car))
+    # print(full_table.shape)
+    # interval = 100000000
+    # start = 0
+    # end = interval
+    # while start < len(v_car):
+    #     table = Parallel(n_jobs=n_jobs)(
+    #         delayed(solve_mpc)(vc_i, xg_i, yg_i, tg_i, vg_i)
+    #         for vc_i, xg_i, yg_i, tg_i, vg_i in tqdm(zip(v_car[start:end], x_goal[start:end], y_goal[start:end], t_goal[start:end], v_goal[start:end]), total=end-start)
+    #     )
+    #     table = np.array(list(filter(lambda item: item is not None, table)))
+    #     if full_table is None:
+    #         full_table = table
+    #     else:
+    #         full_table = np.concatenate((full_table, table), axis=0)
+    #     np.savez(f'goal_mpc_lookup_table_{end}.npz', table=full_table)
+    #     start += interval
+    #     end = min(end + interval, len(v_car))
+    # np.savez('goal_mpc_lookup_table.npz', table=full_table)
     start = 0
-    end = interval
-    while start < len(v_car):
-        table = Parallel(n_jobs=n_jobs)(
-            delayed(solve_mpc)(vc_i, xg_i, yg_i, tg_i, vg_i)
-            for vc_i, xg_i, yg_i, tg_i, vg_i in tqdm(zip(v_car[start:end], x_goal[start:end], y_goal[start:end], t_goal[start:end], v_goal[start:end]), total=end-start)
-        )
-        table = np.array(list(filter(lambda item: item is not None, table)))
-        if full_table is None:
-            full_table = table
-        else:
-            full_table = np.concatenate((full_table, table), axis=0)
-        np.savez(f'goal_mpc_lookup_table_{end}.npz', table=full_table)
-        start += interval
-        end = min(end + interval, len(v_car))
-    np.savez('goal_mpc_lookup_table.npz', table=full_table)
+    end = len(v_car)
+    table = Parallel(n_jobs=n_jobs)(
+        delayed(solve_mpc)(vc_i, xg_i, yg_i, tg_i, vg_i)
+        for vc_i, xg_i, yg_i, tg_i, vg_i in tqdm(zip(v_car[start:end], x_goal[start:end], y_goal[start:end], t_goal[start:end], v_goal[start:end]), total=end-start)
+    )
+    table = np.array(list(filter(lambda item: item is not None, table)))
+    np.savez(f'goal_mpc_lookup_table_tiny_2.npz', table=table)
+    print('Final shape:', table.shape)
+    print('Done')
 
 
 if __name__ == "__main__":
